8658aa3a190d591a069da19ff6fcdf24b54f17dd
BlackJackMC <68107237+BlackJackMC@users.noreply.github.com>
2024-05-16 18:09:09 +0000

    Still trying to revert to 166ea4bd85439e2fffc7e7d15106485e8825ece7

--- src/router/product.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const productController = require("../controller/product");
-const product = express.Router();
-
-product.get("/", productController.getProduct);
-
-module.exports = {
-  product,
-};
